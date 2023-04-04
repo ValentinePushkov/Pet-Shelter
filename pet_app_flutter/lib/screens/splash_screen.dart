90b3750190b9256a4720a694d94ba7aa7bd1b91c
DESKTOP-97687CJ\Valson <harudareaper@gmail.com>
2023-04-04 18:24:14 +0300

    update project

--- pet_app_flutter/lib/screens/splash_screen.dart
+++ pet_app_flutter/lib/screens/splash_screen.dart
@@ -13,49 +13,112 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   int _pageState = 0;
+
+  var _backgourndColor = Colors.white;
+  var _headingColor = Color(0xFFB306060);
+
+  double _headingTop = 100;
+
+  double windowHeight = 0;
+  double windowWidth = 0;
+
+  double _loginWidth = 0;
+
+  double _loginYOffset = 0;
+  double _registerYOffset = 0;
+  double _loginXOffset = 0;
+
+  double _loginOpacity = 1;
+
   @override
   Widget build(BuildContext context) {
+    windowHeight = MediaQuery.of(context).size.height;
+    windowWidth = MediaQuery.of(context).size.width;
+
     switch (_pageState) {
       case 0:
+        _backgourndColor = Colors.white;
+        _headingColor = Color(0xFFB306060);
+
+        _headingTop = 100;
+
+        _loginWidth = windowWidth;
+        _loginOpacity = 1;
+
+        _loginYOffset = windowHeight;
+        _registerYOffset = windowHeight;
+        _loginXOffset = 0;
         break;
       case 1:
+        _backgourndColor = Color(0xFFB306060);
+        _headingColor = Colors.white;
+
+        _headingTop = 90;
+
+        _loginWidth = windowWidth;
+        _loginOpacity = 1;
+
+        _loginYOffset = 270;
+        _registerYOffset = windowHeight;
+        _loginXOffset = 0;
         break;
-      case 3:
+      case 2:
+        _backgourndColor = Color(0xFFB306060);
+        _headingColor = Colors.white;
+
+        _headingTop = 80;
+
+        _loginWidth = windowWidth - 40;
+        _loginOpacity = 0.7;
+
+        _loginYOffset = 250;
+        _registerYOffset = 270;
+        _loginXOffset = 20;
         break;
     }
     return Stack(
       children: [
         AnimatedContainer(
+          duration: Duration(milliseconds: 1000),
           curve: Curves.fastLinearToSlowEaseIn,
-          duration: Duration(milliseconds: 300),
+          color: _backgourndColor,
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  child: Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(top: 128),
-                        child: Text(
-                          "Make A New Friend!",
-                          style: TextStyle(
-                            fontSize: 28,
-                            color: Color(0xFFB306060),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _pageState = 0;
+                    });
+                  },
+                  child: Container(
+                    child: Column(
+                      children: [
+                        AnimatedContainer(
+                          duration: Duration(milliseconds: 1000),
+                          curve: Curves.fastLinearToSlowEaseIn,
+                          margin: EdgeInsets.only(top: _headingTop),
+                          child: Text(
+                            "Make A New Friend!",
+                            style: TextStyle(
+                              fontSize: 28,
+                              color: _headingColor,
+                            ),
                           ),
                         ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        margin: EdgeInsets.all(32),
-                        child: Text(
-                          "Pets have so much love to give and the won't ever stop giving it to you once you let htem into your heart.",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Color(0xFFB306060),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 32),
+                          margin: EdgeInsets.all(20),
+                          child: Text(
+                            "Pets have so much love to give and the won't ever stop giving it to you once you let htem into your heart.",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: _headingColor,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
                 Container(
@@ -63,17 +126,17 @@ class _SplashScreenState extends State<SplashScreen> {
                     child: Image.asset("images/Pets.png"),
                   ),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      if (_pageState != 0) {
-                        _pageState = 0;
-                      } else {
-                        _pageState = 1;
-                      }
-                    });
-                  },
-                  child: Container(
+                Container(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        if (_pageState != 0) {
+                          _pageState = 0;
+                        } else {
+                          _pageState = 1;
+                        }
+                      });
+                    },
                     child: Container(
                       margin: EdgeInsets.only(
                           top: 32, bottom: 100, left: 32, right: 32),
@@ -93,16 +156,46 @@ class _SplashScreenState extends State<SplashScreen> {
                 )
               ]),
         ),
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _pageState = 2;
+            });
+          },
+          child: AnimatedContainer(
+            width: _loginWidth,
+            duration: Duration(milliseconds: 1000),
+            curve: Curves.fastLinearToSlowEaseIn,
+            transform:
+                Matrix4.translationValues(_loginXOffset, _loginYOffset, 1),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(_loginOpacity),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+              ),
             ),
           ),
-          transform: Matrix4.translationValues(0, 270, 1),
-        )
+        ),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _pageState = 1;
+            });
+          },
+          child: AnimatedContainer(
+            duration: Duration(milliseconds: 1000),
+            curve: Curves.fastLinearToSlowEaseIn,
+            transform: Matrix4.translationValues(0, _registerYOffset, 1),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+              ),
+            ),
+          ),
+        ),
       ],
     );
   }
